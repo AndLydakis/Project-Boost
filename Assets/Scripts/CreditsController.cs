6652f5959408e59bbc7cf29b7502ca89ed18d0f3
AndLyd <andlydakis@gmail.com>
2021-01-05 15:18:56 +0000

    Endings screen

---
+++ Assets/Scripts/CreditsController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] Text creditsText; 
+    void Start()
+    {
+        creditsText.text = ("Congratulations!\nYou beat Project Boost!\nYour time was " + PlayerStats.Instance.Time + "!\nThanks for Playing!\nPress Enter to return to main menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return)) {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
